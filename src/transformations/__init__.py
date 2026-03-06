32f48f07d6e501b09897f1fb62dbe84db8c2c166
ola-sk <50403262+ola-sk@users.noreply.github.com>
2026-03-06 12:24:40 +0100

    Add reflection matrix function and update package initialization

--- src/transformations/__init__.py
+++ src/transformations/__init__.py
@@ -2,4 +2,5 @@
 
 # The transformations package contains modular (atomic) transformation matrices for linear algebra operations.
 from .rotation import rotation_matrix
+from .reflection import reflection_matrix
 
