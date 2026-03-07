84aa3432bb9a21d3c0dc0003a18fa56c1155fdc2
ola-sk <50403262+ola-sk@users.noreply.github.com>
2026-03-07 08:08:14 +0100

    Add shearing matrix functions in other planes (xy, yz) for 3D transformations and a more generic function that shears in a single dimension; describe rules of the shearing as we use it in this project. Update imports

--- src/transformations/__init__.py
+++ src/transformations/__init__.py
@@ -5,5 +5,9 @@ from .rotation import rotation_matrix
 from .reflection import reflection_about_axis_matrix
 from .scaling import scaling_matrix
 from .translation import translation_vector
-from .shearing import shear_parallel_to_xz_plane
-
+from .shearing import (
+    shearing_matrix,
+    shear_parallel_to_xy_plane,
+    shear_parallel_to_xz_plane,
+    shear_parallel_to_yz_plane,
+)
